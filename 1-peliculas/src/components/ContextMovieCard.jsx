c2617a9f9997f25d7b1b32d253aa88a1eff546d0
Luis Hanco <86266962+LuisHanco@users.noreply.github.com>
2025-10-07 17:15:24 -0500

    @Luishanco

--- 1-peliculas/src/components/ContextMovieCard.jsx
+++ 1-peliculas/src/components/ContextMovieCard.jsx
@@ -1,9 +1,23 @@
-export function ContextMovieCard(){
+import { useEffect, useState } from "react";
+import { get } from "../data/httpClient";
+import {MovieCard} from "../components/MovieCard"
 
 
-    
+
+    export function ContextMovieCard(){
+         const [movies,SetMovies] =useState([]);
+         useEffect(()=>{
+            get("/discover/movie").then((data)=>{SetMovies(data.results);
+            });
+         }, []);
+
+           return (<ul>
+            {movies.map((movie)=>(
+                <MovieCard/>
+            ))}
+           </ul>);
+    }
 
 
 
-    return (<h1>Context Movie Card</h1>);
-}
+  
