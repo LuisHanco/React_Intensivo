0691cb7b3a60b5a9d504bf602d074cbd7bbcd34d
Luis Hanco <86266962+LuisHanco@users.noreply.github.com>
2025-10-13 06:34:11 -0500

    @Luishanco

--- arcoiris/src/App.jsx
+++ arcoiris/src/App.jsx
@@ -5,6 +5,7 @@ import Programs from './Components/Programs/Programs'
 import Title from './Components/Title/Title'
 import About from './Components/About/About'
 import Campus from './Components/Campus/Campus'
+import Testimonials from './Components/Testimonials/Testimonials'
 
 const App = () => {
   return (
@@ -18,6 +19,7 @@ const App = () => {
         <Title subTitle='Galeria' title='Fotos del Colegio' />
         <Campus/>
         <Title subTitle='Testimonios' title='Que dice el Estudiante' />
+        <Testimonials/>
       </div>
     </div>
   )
--- arcoiris/src/Components/Testimonials/Testimonials.jsx
+++ arcoiris/src/Components/Testimonials/Testimonials.jsx
@@ -1,11 +1,86 @@
 import React from 'react'
 import './Testimonials.css'
+import next_icon from '../../assets/next-icon.png'
+import back_icon from '../../assets/back-icon.png'
+import user_1 from '../../assets/user-1.png'
+import user_2 from '../../assets/user-2.png'
+import user_3 from '../../assets/user-3.png'
+import user_4 from '../../assets/user-4.png'
 
 const Testimonials = () => {
+  
+  const slideForward =() =>{
+
+  }
+
+  const slideBackward = () =>{
+
+  }
+
+
+
   return (
-    <div>
+
+      <div className="testimonials">
+        <img src={next_icon} alt="" className='next-btn' onClick={slideForward} />
+        <img src={back_icon} alt="" className='back-btn' onClick={slideBackward} />
+        <div className="slider">
+          <ul>
+            <li>
+              <div className="slide">
+                <div className="user-info">
+                  <img src={user_1} alt="" />
+                  <div>
+                    <h3>William Jackson</h3>
+                    <span>Edusity usa</span>
+                  </div>
+                </div>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum similique unde ad totam nostrum, perferendis deserunt maxime animi minima iste ratione doloribus dolorem ducimus alias magni, reiciendis voluptatibus nisi veritatis.</p>
+              </div>
+            </li>
+
+            <li>
+              <div className="slide">
+                <div className="user-info">
+                  <img src={user_2} alt="" />
+                  <div>
+                    <h3>William Jackson</h3>
+                    <span>Edusity usa</span>
+                  </div>
+                </div>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum similique unde ad totam nostrum, perferendis deserunt maxime animi minima iste ratione doloribus dolorem ducimus alias magni, reiciendis voluptatibus nisi veritatis.</p>
+              </div>
+            </li>
+
+            <li>
+              <div className="slide">
+                <div className="user-info">
+                  <img src={user_3} alt="" />
+                  <div>
+                    <h3>William Jackson</h3>
+                    <span>Edusity usa</span>
+                  </div>
+                </div>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum similique unde ad totam nostrum, perferendis deserunt maxime animi minima iste ratione doloribus dolorem ducimus alias magni, reiciendis voluptatibus nisi veritatis.</p>
+              </div>
+            </li>
+
+            <li>
+              <div className="slide">
+                <div className="user-info">
+                  <img src={user_4} alt="" />
+                  <div>
+                    <h3>William Jackson</h3>
+                    <span>Edusity usa</span>
+                  </div>
+                </div>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum similique unde ad totam nostrum, perferendis deserunt maxime animi minima iste ratione doloribus dolorem ducimus alias magni, reiciendis voluptatibus nisi veritatis.</p>
+              </div>
+            </li>
+          </ul>
+        </div>
+      </div>
       
-    </div>
   )
 }
 
